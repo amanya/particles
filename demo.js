a119660f28834ae7dacd8f7483cdbac715abc928
Albert Manyà <albertmp@eml.cc>
2014-12-28 18:04:05 +0100

    WIP

--- demo.js
+++ demo.js
@@ -1,6 +1,41 @@
-;(function(){
-var c = document.getElementById('canvas');
-var ctx = c.getContext('2d');
-ctx.fillStyle = "#FFFFFF";
-ctx.fillRect(0, 0, 10, 10);
+(function(){
+    var PARTICLE_SIZE = 10;
+    var GRAVITY = 1.03;
+    var FADE = 0.9;
+
+    var drawParticle = function(p) {
+        this.ctx.fillStyle = p.color;
+        this.ctx.fillRect(p.x, p.y, p.w, p.h);
+    };
+
+    this.canvas = document.getElementById('canvas');
+    this.ctx = this.canvas.getContext('2d');
+
+    var Particle = {
+        x: 0,
+        y: 0,
+        w: 10,
+        h: 10,
+        color: '#FFF'
+    };
+
+    var p = Particle;
+
+    p.x = this.canvas.width / 2 - PARTICLE_SIZE / 2;
+    p.y = this.canvas.height / 2 - PARTICLE_SIZE / 2;
+    p.w = PARTICLE_SIZE;
+    p.h = PARTICLE_SIZE;
+    p.color = '#FFF';
+
+    var gameLoop = function() {
+        this.ctx.clearRect (0 ,0, this.canvas.width, this.canvas.height);
+        drawParticle(p);
+        p.y -= 5;
+        p.y *= GRAVITY;
+        p.w *= FADE;
+        p.h *= FADE;
+    };
+
+    setInterval(gameLoop, 33);
+
 })();
